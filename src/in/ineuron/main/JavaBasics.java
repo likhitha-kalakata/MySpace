336c002716bbdeecf95baf8e6cb6f45d5c4ec7af
Likhitha <klikhitha.kalakata@gmail.com>
2022-11-30 10:03:07 +0530

    demo project

---
+++ src/in/ineuron/main/JavaBasics.java
@@ -0,0 +1,18 @@
+package in.ineuron.main;
+
+public class JavaBasics {
+
+	public static void main(String[] args) {
+		int [] a= {10, 20, 30, 40};
+		for(int ele :a) {
+			System.out.println(ele);
+		}
+		
+		String name ="Likhitha";
+		int count=name.toUpperCase().length();
+		System.out.println("The length of String is :: "+ count);
+ 
+		
+	}
+
+}
